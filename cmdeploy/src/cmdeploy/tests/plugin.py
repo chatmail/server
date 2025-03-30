3dd39a1cc97e896d291527e19dc18f420ab7b9d7
holger krekel <holger@merlinux.eu>
2025-03-31 00:27:27 +0200

    fix skip-comment as requested

--- cmdeploy/src/cmdeploy/tests/plugin.py
+++ cmdeploy/src/cmdeploy/tests/plugin.py
@@ -62,7 +62,7 @@ def sshdomain(maildomain):
 def maildomain2():
     domain = os.environ.get("CHATMAIL_DOMAIN2")
     if not domain:
-        pytest.skip("set CHATMAIL_DOMAIN2 to second chatmail server (FQDN form)")
+        pytest.skip("set CHATMAIL_DOMAIN2 to a second chatmail server")
     return domain
 
 
